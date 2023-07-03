20674405c4c0243fd7bf59f3dd134309b7112ab8
Robert <robert.kedves.90@gmail.com>
2023-07-03 19:10:47 +0300

    ex 5

--- Lab2Ex5/Program.cs
+++ Lab2Ex5/Program.cs
@@ -4,9 +4,26 @@ namespace Lab2Ex5
 {
     internal class Program
     {
+        /*Scrieti o functie care va afisa piramida numerelor pare, intre 2 si n, n fiind citit de la tastatura*/
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Introdu numărul: ");
+            int numar = int.Parse(Console.ReadLine());
+
+            AfiseazaPiramidaNumarPar(numar);
+        }
+
+        static void AfiseazaPiramidaNumarPar(int numar)
+        {
+            for (int i = 2; i <= numar; i += 2)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
