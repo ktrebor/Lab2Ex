f48ff939a12eb25b713fd88f0bdd14bcfc61f4f9
Robert <robert.kedves.90@gmail.com>
2023-07-03 18:58:17 +0300

    ex 4

--- Lab2Ex4/Program.cs
+++ Lab2Ex4/Program.cs
@@ -4,9 +4,31 @@ namespace Lab2Ex4
 {
     internal class Program
     {
+        /*Scrieti o functie care va determina daca un numar este sau nu patrat perfect. Apelati o si afisati i rezultatul*/
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Introdu numărul: ");
+            int numar = int.Parse(Console.ReadLine());
+
+            if (EstePatratPerfect(numar))
+            {
+                Console.WriteLine("Numărul ESTE pătrat perfect.");
+            }
+            else
+            {
+                Console.WriteLine("Numărul NU ESTE pătrat perfect.");
+            }
+        }
+
+        static bool EstePatratPerfect(int numar)
+        {
+            double patratulNumarului = Math.Sqrt(numar);
+
+            if (patratulNumarului - Math.Floor(patratulNumarului) == 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
